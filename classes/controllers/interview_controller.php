aba885331bd0cfd9d52fd85a0b03dc8a080e1f66
root <root@gap-ubuntu>
2015-09-08 18:06:17 +1000

    make client send interview data on server

--- classes/controllers/interview_controller.php
+++ classes/controllers/interview_controller.php
@@ -3,9 +3,19 @@ class interview_controller extends controller
 {
     public function get_questions_action($params)
     {
-        $result = db::exec($this->db, "SELECT id, content FROM i_questions", null);
-
-        $this->view->render('json', $result);
+        $q = db::exec($this->db, "SELECT id, type, content FROM i_questions", null);
+        $this->view->render('json', $q);
     }
     
+    public function get_answears_action($params)
+    {
+        $a = db::exec($this->db, "SELECT id, question_id, content FROM i_answears", null);
+        $this->view->render('json', $a);
+    }
+    
+    public function save_interview_action($params)
+    {
+        $request_body = file_get_contents('php://input');
+        $this->view->render('json', json_decode($request_body, true));
+    }
 }
