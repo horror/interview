a0c9db8b8dab66d745ea2bb1bd280762744d5750
root <root@gap-ubuntu>
2015-09-11 09:05:05 +1000

    fix header css path

--- views/header.php
+++ views/header.php
@@ -2,8 +2,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-    <link type="text/css" href="/css/normalize.css" rel="stylesheet" />
-    <link type="text/css" href="/css/foundation.min.css" rel="stylesheet" />
+    <link type="text/css" href="/vendors/css/normalize.css" rel="stylesheet" />
+    <link type="text/css" href="/vendors/css/foundation.min.css" rel="stylesheet" />
     <link type="text/css" href="/css/custom.css" rel="stylesheet" />
     <title>Аутентификация</title>
 </head>
